b4abe50f299e8a426685cfe099f9ffdfddf7dee5
Nathan Getty <ngetty@wawanesa.com>
2019-09-06 08:17:00 -0500

    updated w/ healthcheck

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ class PublishLogs(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
--- healthcheck.py
+++ healthcheck.py
@@ -3,7 +3,10 @@ from requests import get
 url = "http://localhost:80/log/pub"
 
 try:
-    get(url)
+    r = get(url)
+    if not r.ok:
+        print(f"Error, Non-200 Code {r.status_code}")
+
 except Exception as msg:
     print("Error connecting to endpoint.")
     print(f"exception: {msg}")
